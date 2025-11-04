41574fd74d343fa06b19899d26e4f4131540b2ba
alpha dev <alpha5611331@gmail.com>
2025-11-04 21:18:33 +0100

    done export config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: '..\\backend\\static',
 };
 
 export default nextConfig;
